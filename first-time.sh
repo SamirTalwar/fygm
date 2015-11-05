a66fbe427f1c8c6fceef1c4fca641e7cc7ce8524
Samir Talwar <samir@noodlesandwich.com>
2015-11-05 17:40:49 +0000

    first-time.sh.

---
+++ first-time.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Meta
+brew install brew-cask
+
+# Shell
+brew install \
+    autojump \
+    bash \
+    mobile-shell \
+    the_silver_searcher \
+    thefuck \
+    tmux \
+    tree \
+    watch \
+    zsh
+
+# Core Tools
+brew install \
+    coreutils \
+    curl \
+    dos2unix \
+    fswatch \
+    gnu-sed \
+    gnupg \
+    htop-osx \
+    httpie \
+    jq \
+    moreutils \
+    unrar \
+    wget
+
+# Text Editing
+brew install \
+    macvim
+brew install --HEAD \
+    neovim/neovim/neovim
+
+# Image Manipulation
+brew install \
+    imagemagick \
+    pngcrush
+
+# Development
+brew install \
+    carthage \
+    heroku-toolbelt \
+    leiningen \
+    maven \
+    mercurial \
+    python \
+    python3 \
+    sbt \
+    sqlite
+brew cask install \
+    haskell-platform
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# Containerisation
+brew cask install \
+    virtualbox
+brew install \
+    docker \
+    docker-compose \
+    docker-machine
