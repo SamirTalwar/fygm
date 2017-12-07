67602e4453e01f803fa644323ee20a6ed4ff3d42
Samir Talwar <samir@noodlesandwich.com>
2017-12-07 17:31:02 +0000

    Revert "Use nix wherever possible."
    This reverts commit df9d280f8cb4d13d538049809cc026eecde9961f.
    
    Turns out it's more trouble than it's worth.

--- install-apps.mac.sh
+++ install-apps.mac.sh
@@ -5,82 +5,84 @@ set -x
 
 export PATH="/usr/local/bin:$PATH"
 
-command -v nix-env >& /dev/null || curl -fsS https://nixos.org/nix/install | sh
-
 command -v brew >& /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/fonts
 
 command -v rustup >& /dev/null || (curl https://sh.rustup.rs -fsS | sh -s -- -y)
 
-nix-channel --update
-nix-env --upgrade
-
 brew update
 brew upgrade
 
-nix-env --install --attr \
-  nixpkgs.ag \
-  nixpkgs.autojump \
-  nixpkgs.bash \
-  nixpkgs.mosh \
-  nixpkgs.tmux \
-  nixpkgs.tree \
-  nixpkgs.urlview \
-  nixpkgs.watch \
-  nixpkgs.zsh \
-  nixpkgs.zsh-completions
+# Shell
+brew install \
+  autojump \
+  bash \
+  direnv \
+  fzf \
+  mobile-shell \
+  the_silver_searcher \
+  tmux \
+  tree \
+  urlview \
+  watch \
+  zsh
 if [[ ! -e ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Core Tools
-nix-env --install --attr \
-  nixpkgs.coreutils-prefixed \
-  nixpkgs.curl \
-  nixpkgs.dos2unix \
-  nixpkgs.findutils \
-  nixpkgs.htop \
-  nixpkgs.httpie \
-  nixpkgs.jq \
-  nixpkgs.moreutils \
-  nixpkgs.socat \
-  nixpkgs.wget
 brew install \
+  coreutils \
+  curl \
+  dos2unix \
+  findutils \
+  gawk \
+  gnu-sed \
   gnupg \
-  pinentry-mac
+  htop-osx \
+  httpie \
+  httping \
+  jq \
+  moreutils \
+  pinentry \
+  pinentry-mac \
+  socat \
+  unrar \
+  wget
 brew cask install xquartz
 
 # Terminal Fu
-nix-env --install --attr \
-  nixpkgs.entr \
-  nixpkgs.fswatch
-brew install terminal-notifier
+brew install \
+  entr \
+  fswatch \
+  terminal-notifier
 
 # Image Manipulation
-nix-env --install --attr \
-  nixpkgs.imagemagick \
-  nixpkgs.pngcrush
+brew install \
+  imagemagick \
+  pngcrush
 brew cask install gimp
 
 # Development
-nix-env --install --attr \
-  nixpkgs.cmake \
-  nixpkgs.elmPackages.elm \
-  nixpkgs.git \
-  nixpkgs.go \
-  nixpkgs.mercurial \
-  nixpkgs.nodejs-8_x \
-  nixpkgs.ocaml \
-  nixpkgs.opam \
-  nixpkgs.shellcheck \
-  nixpkgs.sqlite \
-  nixpkgs.stack
 brew install \
+  carthage \
+  cmake \
+  emacs \
+  elm \
+  git \
+  go \
+  haskell-stack \
   heroku-toolbelt \
   llvm \
+  mercurial \
+  node \
+  ocaml \
+  opam \
   python \
   python3 \
   ruby \
+  shellcheck \
+  sqlite \
   tidy-html5 \
   yarn
 brew cask install font-fira-code
@@ -95,26 +97,22 @@ EOF
 
 # Java Development
 brew cask install java
-nix-env --install --attr \
-  nixpkgs.groovy \
-  nixpkgs.gradle \
-  nixpkgs.maven \
-  nixpkgs.scala \
-  nixpkgs.sbt
-brew install leiningen
+brew install \
+  groovy \
+  gradle \
+  leiningen \
+  maven \
+  scala \
+  sbt
 
 # Text Editing
-nix-env --install --attr \
-  nixpkgs.emacs \
-  nixpkgs.pandoc \
-  nixpkgs.vim
-
 brew install neovim/neovim/neovim
 pip2 install neovim
 pip3 install neovim
 gem install neovim
+(cd /usr/local/bin && ln -sf nvim vim)
 
-brew cask install emacs
+brew install pandoc
 
 # Containerisation
 brew cask install \
@@ -131,8 +129,6 @@ fi
 if [[ "$(dscl . -read "/Users/$USER" UserShell | field 2)" != '/usr/local/bin/zsh' ]]; then
   sudo chsh -s /usr/local/bin/zsh "$USER"
 fi
-
-nix-collect-garbage --delete-old
 
 brew linkapps
 brew cleanup
