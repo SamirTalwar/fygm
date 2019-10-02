8040799c51c3c281dd36dae25f1fdba88bafd060
Samir Talwar <samir@noodlesandwich.com>
2019-10-02 17:44:50 +0100

    Nix: Move most packages from Homebrew over to Nix.

---
+++ dotfiles/nixpkgs/home.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    # Core
+    pkgs.stdenv
+    pkgs.coreutils
+    pkgs.findutils
+    pkgs.moreutils
+    pkgs.curl
+    pkgs.dos2unix
+    pkgs.gawk
+    pkgs.gnupg
+    pkgs.gnused
+    pkgs.hello
+    pkgs.httpie
+    pkgs.httping
+    pkgs.jq
+    pkgs.lastpass-cli
+    pkgs.ncdu
+    pkgs.socat
+    pkgs.tree
+    pkgs.wget
+
+    # Shell
+    pkgs.autojump
+    pkgs.bash
+    pkgs.direnv
+    pkgs.entr
+    pkgs.fswatch
+    pkgs.fzf
+    pkgs.mosh
+    pkgs.silver-searcher
+    pkgs.terminal-notifier
+    pkgs.tmux
+    pkgs.urlview
+    pkgs.watch
+    pkgs.zsh
+
+    # Image Manipulation
+    pkgs.imagemagick
+    pkgs.pngcrush
+
+    # Editors
+    pkgs.aspell
+    pkgs.pandoc
+
+    # Development
+    pkgs.cmake
+    pkgs.gcc
+    pkgs.git
+    pkgs.gnumake
+    pkgs.go
+    pkgs.llvm
+    pkgs.mercurial
+    pkgs.nixpkgs-fmt
+    pkgs.nodejs
+    pkgs.ocaml
+    pkgs.ocamlPackages.ocamlbuild
+    pkgs.opam
+    pkgs.pipenv
+    pkgs.python
+    pkgs.python3
+    pkgs.python3Packages.pip
+    pkgs.ruby
+    pkgs.rustup
+    pkgs.shellcheck
+    pkgs.sqlite
+    pkgs.stack
+    pkgs.yarn
+
+    # Java Development
+    pkgs.openjdk
+    pkgs.scala
+    pkgs.sbt
+
+    # Cloud Development
+    pkgs.awscli
+    pkgs.google-cloud-sdk
+    pkgs.kubectl
+  ];
+}
--- install-apps.mac.sh
+++ install-apps.mac.sh
@@ -1,140 +1,56 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 set -e
+set -u
 set -x
 
-export PATH="${HOME}/.cabal/bin:${HOME}/.cargo/bin:${HOME}/.ghcup/bin:/usr/local/opt/ruby/bin:/usr/local/bin:${PATH}"
-
-command -v brew >& /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/fonts
+if ! command -v brew >& /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew tap homebrew/cask-fonts
 
 brew update
 brew upgrade
 brew cask upgrade
 
+# macOS applications
+brew cask install \
+  beaker-browser \
+  docker \
+  emacs \
+  etcher \
+  firefox \
+  font-fira-code \
+  font-source-code-pro \
+  jetbrains-toolbox \
+  google-chrome \
+  gpg-suite \
+  keybase \
+  ngrok \
+  nordvpn \
+  scroll-reverser \
+  sketch \
+  transmission \
+  visual-studio \
+  visual-studio-code \
+  vlc \
+  xquartz
 
-# Shell
-brew cask install font-fira-code
-
+# Packages that can't be handled with Nix
 brew install \
-  autojump \
-  bash \
-  direnv \
-  fzf \
-  mosh \
-  the_silver_searcher \
-  tmux \
-  tree \
-  urlview \
-  watch \
-  zplug \
-  zsh
+  neovim \
+  swi-prolog \
+  swiftformat \
+  zplug
+
+brew cleanup
+
+# tmux plugins
 if [[ ! -e ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-# Core Tools
-brew install \
-  coreutils \
-  curl \
-  dos2unix \
-  findutils \
-  gawk \
-  gnu-sed \
-  gnupg \
-  htop-osx \
-  httpie \
-  httping \
-  ispell \
-  jq \
-  lastpass-cli \
-  moreutils \
-  ncdu \
-  pinentry \
-  pinentry-mac \
-  socat \
-  unrar \
-  wget
-brew cask install xquartz
-
-# Terminal Fu
-brew install \
-  entr \
-  fswatch \
-  terminal-notifier
-
-# macOS
-brew cask install scroll-reverser
-
-# Image Manipulation
-brew install \
-  imagemagick \
-  pngcrush
-
-# Development
-brew install make --with-default-names
-brew install \
-  carthage \
-  cmake \
-  dep \
-  elm \
-  gcc \
-  git \
-  go \
-  llvm \
-  mercurial \
-  node \
-  pipenv \
-  python \
-  python3 \
-  ruby \
-  shellcheck \
-  sqlite \
-  swiftformat \
-  tidy-html5 \
-  yarn
-
-# Cloud Development
-brew cask install \
-  docker \
-  google-cloud-sdk
-brew install \
-  awscli \
-  docker-completion \
-  docker-compose-completion
-pip3 install --user --upgrade \
-  aws-sam-cli
-gcloud components install -q alpha beta kubectl
-
-# Java Development
-brew cask install java
-brew install \
-  groovy \
-  gradle \
-  leiningen \
-  maven \
-  scala \
-  sbt
-brew cask install intellij-idea
-
-# Haskell Development
-command -v ghcup >& /dev/null || (
-  export BOOTSTRAP_HASKELL_NONINTERACTIVE=true
-  curl https://get-ghcup.haskell.org -sSf | sh
-)
-cabal new-install cabal-install
-brew install haskell-stack
-
-# OCaml Development
-brew install opam
-opam init --no-setup
-opam upgrade
-opam install \
-  ocaml \
-  ocamlbuild
-
-# Prolog Development
-brew install swi-prolog --with-libarchive
+# SWI Prolog packs
 mkdir -p ~/.config/swipl/packs
 swipl <<EOF
 expand_file_name('~/.config/swipl/packs', [PacksDirectory]),
@@ -144,27 +60,11 @@ pack_install(regex, [upgrade(true), interactive(false)]).
 halt.
 EOF
 
-# Rust Development
-command -v rustup >& /dev/null || (
-  curl https://sh.rustup.rs -fsS | sh -s -- -y
+# configure the user shell
+CURRENT_SHELL=$(
+  dscl . -read /Users/${USER} UserShell \
+    | cut -d ' ' -f 2
 )
-
-# Editors
-brew install
-  aspell \
-  neovim \
-  pandoc
-brew cask install \
-  emacs
-pip2 install --user pynvim sexpdata websocket-client
-pip3 install --user pynvim sexpdata websocket-client
-gem install neovim
-
-if ! grep -F /usr/local/bin/zsh /etc/shells; then
-  sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
+if [[ $CURRENT_SHELL != ${HOME}/.nix-profile/bin/zsh ]]; then
+  sudo chsh -s ${HOME}/.nix-profile/bin/zsh $USER
 fi
-if [[ "$(dscl . -read "/Users/$USER" UserShell | cut -d ' ' -f 2)" != '/usr/local/bin/zsh' ]]; then
-  sudo chsh -s /usr/local/bin/zsh "$USER"
-fi
-
-brew cleanup
