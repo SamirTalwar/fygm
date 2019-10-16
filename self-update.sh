fb9a31e83ef59d1c777714974c305a4da59659b5
Samir Talwar <samir@noodlesandwich.com>
2019-10-16 10:34:38 +0100

    Self-update if submodules need updating.

--- self-update.sh
+++ self-update.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env zsh
 
-root=$(realpath ${0:h})
+root=${0:h:a}
 
 function self-update {
   local output exit_code
+
   output=$(cd $root && git pull --ff-only 2>&1)
   exit_code=$?
   if [[ $exit_code -ne 0 ]]; then
+    echo '$ git pull --ff-only'
     echo "$output"
     echo "Updating $root failed with status code $exit_code."
     exit $exit_code
   fi
 
   if ! [[ $output =~ '^Already up[ -]to[ -]date.$' ]]; then
+    echo "$output"
+  else
+    return 1
+  fi
+
+  output=$(cd $root && git submodule update --init --recursive 2>&1)
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    echo '$ git submodule update --init --recursive'
+    echo "$output"
+    echo "Updating $root failed with status code $exit_code."
+    exit $exit_code
+  fi
+
+  if [[ -n $output ]]; then
     echo "$output"
   else
     return 1
