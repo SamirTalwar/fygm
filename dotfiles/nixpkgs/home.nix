823a1458c734a23f52bc350e826796f2411a9c48
Samir Talwar <samir@noodlesandwich.com>
2020-10-26 14:59:24 +0100

    Set up Alacritty with Nix, using nixGL.

--- dotfiles/nixpkgs/home.nix
+++ dotfiles/nixpkgs/home.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
-
+let
+  nixGL = (import
+    (pkgs.fetchFromGitHub {
+      owner = "guibou";
+      repo = "nixGL";
+      rev = "fad15ba09de65fc58052df84b9f68fbc088e5e7c";
+      sha256 = "1wc5gfj5ymgm4gxx5pz4lkqp5vxqdk2njlbnrc1kmailgzj6f75h";
+    })
+    { }).nixGLDefault;
+  alacritty = pkgs.writeScriptBin "alacritty" ''
+    #!${pkgs.stdenv.shell}
+    exec ${nixGL}/bin/nixGL ${pkgs.alacritty}/bin/alacritty
+  '';
+in
 with pkgs;
 {
   # Let Home Manager install and manage itself.
@@ -90,6 +103,7 @@ with pkgs;
       terminal-notifier
     ]
     else [
+      alacritty
       traceroute
       xclip
     ]
