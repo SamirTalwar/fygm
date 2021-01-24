59c120702d6de55dc4b47c0b37f1ab024a68a18c
Samir Talwar <samir@noodlesandwich.com>
2021-01-24 15:42:46 +0100

    install: Mac App Store applications, with `mas`.

--- install-apps.mac.sh
+++ install-apps.mac.sh
@@ -4,6 +4,39 @@ set -e
 set -u
 set -o pipefail
 
+fonts=(
+  font-fira-code
+  font-source-code-pro
+)
+
+brew_cli_programs=(
+  mas
+  reattach-to-user-namespace
+)
+
+brew_mac_applications=(
+  alacritty
+  dash
+  discord
+  docker
+  dropbox
+  emacs-mac-spacemacs-icon
+  firefox
+  google-chrome
+  nordvpn
+  signal
+  sync
+  visual-studio-code
+  vlc
+)
+
+mac_app_store_applications=(
+  485812721 # TweetDeck
+  585829637 # Todoist
+  692867256 # Simplenote
+  926036361 # LastPass
+)
+
 source ${0:A:h}/common.sh
 
 if ! command -v brew >& /dev/null; then
@@ -21,28 +54,7 @@ brew upgrade
 
 installed_casks=($(brew list --cask))
 
-fonts=(
-  font-fira-code
-  font-source-code-pro
-)
-
-applications=(
-  alacritty
-  dash
-  discord
-  docker
-  dropbox
-  emacs-mac-spacemacs-icon
-  firefox
-  google-chrome
-  nordvpn
-  signal
-  sync
-  visual-studio-code
-  vlc
-)
-
-function cask_install {
+function brew_cask_install {
   to_install=()
   for app in $@; do
     if [[ ${installed_casks[(ie)$app]} -gt ${#installed_casks} ]]; then
@@ -54,8 +66,12 @@ function cask_install {
   fi
 }
 
-now 'Installing new packages'
-cask_install $fonts $applications
+now 'Installing new Homebrew packages'
+brew install $brew_cli_programs
+brew_cask_install $fonts $brew_mac_applications
+
+now 'Installing Mac App Store applications'
+reattach-to-user-namespace mas install $mac_app_store_applications
 
 now 'Cleaning up'
 brew cleanup
