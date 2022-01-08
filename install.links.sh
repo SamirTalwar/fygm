5cecc13975b5038de3dbd93b49494f8fec80c617
Samir Talwar <samir@noodlesandwich.com>
2022-01-08 13:02:50 +0100

    Sway: Fix the symlink.

--- install.links.sh
+++ install.links.sh
@@ -25,7 +25,7 @@ links=(
   ~/.config/nixpkgs/home.nix $dotfiles/nixpkgs/home.nix
   ~/.config/nvim/coc-settings.json $dotfiles/nvim/coc-settings.json
   ~/.config/nvim/init.vim $dotfiles/nvim/init.vim
-  ~/.config/sway/config $dotfiles/sway/config
+  ~/.config/sway $dotfiles/sway
   ~/.config/wofi $dotfiles/wofi
   ~/.emacs.d $dotfiles/emacs.d
   ~/.ghc/ghci.conf $dotfiles/ghc/ghci.conf
