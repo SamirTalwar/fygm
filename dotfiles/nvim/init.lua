10eacedf68109da834389aa8a60499288bf5658e
Samir Talwar <samir@functional.computer>
2023-03-11 22:51:04 +0100

    vim: Install some basic plugins.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -11,3 +11,32 @@ vim.opt.softtabstop = 2 -- insert 2 spaces when typing <Tab>
 -- Search
 vim.opt.ignorecase = true -- ignore case when searching
 vim.opt.smartcase = true -- stop ignoring case when uppercase is used
+
+-- Leader
+vim.g.mapleader = " " -- set the leader key to <Space>
+
+-- Set up lazy.nvim, the plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Initialize plugins
+local plugins = {
+  { "folke/which-key.nvim" }, -- show keybinding help as you type
+  { "itchyny/lightline.vim", lazy = false }, -- a useful status bar
+
+  { "tpope/vim-repeat" }, -- better repeat (`.`) semantics
+  { "tpope/vim-surround" }, -- add, remove, and modify surrounding characters
+
+  { "christoomey/vim-tmux-navigator" }, -- navigate tmux easily
+}
+require("lazy").setup(plugins)
