0549eb7712da3eab9b342daeeec616aaff5d6859
Samir Talwar <samir@functional.computer>
2024-07-01 17:08:16 +0200

    wezterm: Open new tabs and windows in the home directory.

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -23,4 +23,45 @@ config.font_size = 16
 -- on macOS, allow composing characters with Left Alt
 config.send_composed_key_when_left_alt_is_pressed = true
 
+-- helper for simpler keybindings which can have different combinations of mods
+-- (see below for examples)
+config.keys = {}
+function key(opts)
+  local all_mods
+  if type(opts.mods) == 'table' then
+    all_mods = opts.mods
+  else
+    all_mods = { opts.mods }
+  end
+  for _, mods in pairs(all_mods) do
+    local key = opts.key
+    for match in string.gmatch(opts.key, '%a+') do
+      if match == 'SHIFT' then
+        key = string.upper(key)
+      end
+    end
+    table.insert(config.keys, {
+      key = opts.key,
+      mods = mods,
+      action = opts.action,
+    })
+  end
+end
+
+-- open new tabs or windows in the home directory
+key {
+  key = 't',
+  mods = { 'SUPER', 'CTRL|SHIFT' },
+  action = wezterm.action.SpawnCommandInNewTab {
+    cwd = wezterm.home_dir,
+  },
+}
+key {
+  key = 'n',
+  mods = { 'SUPER', 'CTRL|SHIFT' },
+  action = wezterm.action.SpawnCommandInNewWindow {
+    cwd = wezterm.home_dir,
+  },
+}
+
 return config
