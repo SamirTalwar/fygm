2903266b924955145400334d5f3c09dc9cb6f3b8
Samir Talwar <samir@functional.computer>
2024-07-01 10:01:44 +0200

    wezterm: Config for persistence, scrolling, and macOS keyboards.

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -2,9 +2,25 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+---- persistence
+config.unix_domains = {
+  { name = 'unix' },
+}
+config.default_gui_startup_args = { 'connect', 'unix' }
+
+---- color scheme
 config.color_scheme = 'tokyonight'
 
+---- UI
+config.enable_scroll_bar = true
+config.scrollback_lines = 20000
+
+---- fonts
 config.font = wezterm.font('Iosevka Term', { weight = 'Light' })
 config.font_size = 16
+
+---- keyboard
+-- on macOS, allow composing characters with Left Alt
+config.send_composed_key_when_left_alt_is_pressed = true
 
 return config
